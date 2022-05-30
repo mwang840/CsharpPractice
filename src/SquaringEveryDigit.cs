1d6e0cf7afde609493e8abb252f746189684ce44
Maxwell Wang <maxwang@udel.edu>
2022-05-29 20:40:09 -0400

    cleaned up code, terneary needed

--- src/SquaringEveryDigit.cs
+++ src/SquaringEveryDigit.cs
@@ -3,9 +3,15 @@ using System;
 namespace Kata{
     public static int SquareDigits(int n){
         int Squared = 0;
-        string toSquare = ToString(n);
-        for(int i = 0; i < toSquare.Length; ++i){
-            Squared += ToInt32(POW(toSquare[i], 2));
+        int digitPlace = 0;
+        while(n > 0){
+            int grab = n % 10;
+            int sq = grab * grab;
+            
+            Squared+= (int)Math.Pow(10, digitPlace) * sq;
+            digitPlace += sq > 10 ? 2:1;
+
+            n/= 10;
         }
         return Squared;
     }

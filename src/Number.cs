a57f1caea19579bbf123998a70aa71acc835bddc
Maxwell Wang <maxwang@udel.edu>
2022-06-22 18:39:52 -0400

    nice helper function

--- src/Number.cs
+++ src/Number.cs
@@ -2,13 +2,25 @@ using System;
 
 namespace Number{
     public class Number{
+        
         public int DigitalRoot(long n){
-            if(n < 10){
-                return (int)n;
-            }
-            else{
-                return DigitalRoot((n % 10) + DigitalRoot((int)Math.Floor(n % 10)));
-            }
+           while(n >= 10){
+            n = singleNum(n);
+           }
+           return (int)n;
+        }
+        public int singleNum(long n){
+           if(n <= 9){
+            return (int)n;
+           }
+           else{
+            return singleNum(n/10) + (int)(n % 10);
+           }
+        }
+
+        static void Main(string[] args){
+            Number n = new Number();
+            Console.WriteLine(n.DigitalRoot(493193));
         }
     }
 }

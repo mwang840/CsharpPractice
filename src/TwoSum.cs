482358ea4e47cbf161e5e7980118196b9318b3ff
Maxwell Wang <maxwang@udel.edu>
2022-07-06 07:59:43 -0400

    twosums

---
+++ src/TwoSum.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Sums{
+
+    public static int[] TwoSum(int []numbers, int target){
+        return new int[0];
+    }
+
+
+    static void AMain(string[]args){
+        Console.Write("ayo the pizza here");
+    }
+}

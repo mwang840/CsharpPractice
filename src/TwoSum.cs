42e29bf09f90a361690b183681386c512ecf34e2
mwang840 <maxwang@udel.edu>
2022-07-06 15:28:15 -0400

    two sums

--- src/TwoSum.cs
+++ src/TwoSum.cs
@@ -3,11 +3,20 @@ using System;
 public class Sums{
 
     public static int[] TwoSum(int []numbers, int target){
+        int[] twoTargets = new int[2];
+        for(int i = 0; i < numbers.Length; ++i){
+            for(int j = i+1; j < numbers.Length; ++j){
+                int consecutiveSum = numbers[i] + numbers[j];
+                if(consecutiveSum == target){
+                    return new int[2]{i,j};
+                }
+            }
+        }
         return new int[0];
     }
 
 
     static void AMain(string[]args){
-        Console.Write("ayo the pizza here");
+        Console.Write("Hello");
     }
 }
